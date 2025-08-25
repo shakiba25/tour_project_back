33273446f9685b3555498ca937f548673cd33429
shakiba <shakiba25.kh@gmail.com>
2025-08-25 16:54:21 +0330

    admin panel

--- tours/admin.py
+++ tours/admin.py
@@ -1,5 +1,50 @@
 from django.contrib import admin
-from .models import Tour
-# Register your models here.
+from .models import Tour, Hotel, FlightInfo, Service, ItineraryItem, Image
 
-admin.site.register(Tour)
+
+# class ImageInline(admin.TabularInline):  # یا StackedInline برای ظاهر ستونی
+#     model = Tour.images.through          # چون ManyToMany هست
+#     extra = 1
+
+
+class ServiceInline(admin.TabularInline):
+    model = Tour.services.through
+    extra = 1
+
+
+class ItineraryInline(admin.TabularInline):
+    model = Tour.itinerary.through
+    extra = 1
+
+
+@admin.register(Tour)
+class TourAdmin(admin.ModelAdmin):
+    list_display = ("tour_id", "name", "destination", "price", "insurance_included")
+    inlines = [ ServiceInline, ItineraryInline] #ImageInline,
+    search_fields = ("name", "destination")
+    list_filter = ("destination", "insurance_included")
+
+
+# @admin.register(Image)
+# class ImageAdmin(admin.ModelAdmin):
+#     list_display = ("id", "file")
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ("name", "star")
+
+
+@admin.register(FlightInfo)
+class FlightInfoAdmin(admin.ModelAdmin):
+    list_display = ("airline", "date", "time")
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+@admin.register(ItineraryItem)
+class ItineraryItemAdmin(admin.ModelAdmin):
+    list_display = ("description",)
